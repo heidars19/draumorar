b05385229e4d2acdb9809dc6204edc146be14442
heidars19 <heidars19@ru.is>
2020-05-27 15:19:22 +0000

    lots of stuff

---
+++ draumorar/staticfiles/js/main.js
@@ -0,0 +1,11 @@
+// $(document).ready(function() {
+//     $(window).scroll(function() {
+//         if ($(this).scrollTop() > 1) {
+//             $(".parallax").css({"opacity": "0.3"})
+//         }
+//         else {
+//             $(".parallax").css({"opacity": "1"})
+//         }
+//     })
+// })
+
